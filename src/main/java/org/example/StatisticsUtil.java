29cab64e6d30a4d94f1cfbc9edc676d9b29fa223
Michael <mihali5@mail.ru>
2025-03-23 16:27:30 +0300

    logging

--- src/main/java/org/example/StatisticsUtil.java
+++ src/main/java/org/example/StatisticsUtil.java
@@ -8,15 +8,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalDouble;
 import java.util.Set;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 import java.util.stream.Collectors;
 
 public class StatisticsUtil {
+    private static final Logger logger = Logger.getLogger(StatisticsUtil.class.getName());
 
     private StatisticsUtil() {
     }
 
     public static List<Statistics> createStatistics(List<Student> students,
                                                     List<University> universities) {
+
+        logger.log(Level.INFO, "Statistics module started");
 
         List<Statistics> statisticsList = new ArrayList<>();
 
@@ -51,6 +56,9 @@ public class StatisticsUtil {
             avgExamScore.ifPresent(value -> statistics.setAvgExamScore(
                     (float) BigDecimal.valueOf(value).setScale(2, RoundingMode.HALF_UP).doubleValue()));
         });
+
+        logger.log(Level.INFO,
+                String.format("Statistics module finished with %s statistical objects", statisticsList.size()));
 
         return statisticsList;
     }
